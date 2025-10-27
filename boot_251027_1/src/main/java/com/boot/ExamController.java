57c131b1063e4a721d41ade0e017216d8a518fa2
Rootplant <ho7465@gmail.com>
2025-10-27 16:34:38 +0900

    Update ExamController.java

--- boot_251027_1/src/main/java/com/boot/ExamController.java
+++ boot_251027_1/src/main/java/com/boot/ExamController.java
@@ -11,7 +11,7 @@ public class ExamController {
 	@RequestMapping("/")
 //	public void home() {
 	public String home() {
-		log.info("hello boot123456");
+		log.info("hello boot1234566");
 		
 		return "hello";
 	}

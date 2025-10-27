1b965bc29307e453588630009257f8175eed116b
Rootplant <ho7465@gmail.com>
2025-10-27 16:19:44 +0900

    4

--- boot_251027_1/src/main/java/com/boot/ExamController.java
+++ boot_251027_1/src/main/java/com/boot/ExamController.java
@@ -11,7 +11,7 @@ public class ExamController {
 	@RequestMapping("/")
 //	public void home() {
 	public String home() {
-		log.info("hello boot12345");
+		log.info("hello boot123452");
 		
 		return "hello";
 	}

073489319dcf82df3283f98ddf28588bf45d2103
Rootplant <ho7465@gmail.com>
2025-10-27 16:12:37 +0900

    123

---
+++ boot_251027_1/src/main/java/com/boot/Boot2510271Application.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2510271Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot2510271Application.class, args);
+	}
+
+}
---
+++ boot_251027_1/src/test/java/com/boot/Boot2510271ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot2510271ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
